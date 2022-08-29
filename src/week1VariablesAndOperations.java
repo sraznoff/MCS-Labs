b18d14c27c3a12380410d7a01b1aadb893f37300
Sraznoff <sraznoff@headwatersgeographic.com>
2022-08-28 21:34:25 -0600

    Pushing saved code changes to git

--- src/week1VariablesAndOperations.java
+++ src/week1VariablesAndOperations.java
@@ -3,7 +3,16 @@ public class week1VariablesAndOperations {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//create a variable to hold the quantity of available plane seats left on a flight
+		int availablePlaneSeats = 5;
+		//create a variable to hold the total cost of groceries at a checkout;
+		double groceryBill = 42.31;
+		//create a variable to hold a person's middle initial
+		char middleInitial = 'e';
+		//create a variable to hold true if its hot outside and false if its cold outside
+		boolean isHot = false;
+		//create a variable to hold a person's first name
+		String firstName = "Gerald";
 	}
 
 }
